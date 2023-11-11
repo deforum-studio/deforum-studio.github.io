305b7e6940deb3c3581e1b043c4b5a565798b7e4
deforum <deforum.art@gmail.com>
2023-11-10 16:19:34 -0800

    update video and mesh

--- scripts/noise.js
+++ scripts/noise.js
@@ -1,17 +1,48 @@
+// Get the canvas and its context
 const canvas = document.getElementById('noiseCanvas');
 const ctx = canvas.getContext('2d');
-const width = canvas.width;
-const height = canvas.height;
-let imageData = ctx.createImageData(width, height);
-let data = imageData.data;
+
+// Define the parameters
+let gridHeight = 4; // Change as needed, in pixels
+let gridWidth = 4; // Change as needed, in pixels
+let markHeight = 6; // Change as needed, in pixels
+let markWidth = 2; // Change as needed, in pixels
+let markColor = [0, 0, 0, 255]; // RGBA color of the mark
+let markOpacity = 1; // Opacity of the mark
+let offset = 2; // Change as needed, in pixels
+
+
+// Function to resize the canvas and regenerate the image data
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    imageData = ctx.createImageData(canvas.width, canvas.height);
+    data = imageData.data;
+}
+
+// Call resizeCanvas when the window is resized
+window.addEventListener('resize', resizeCanvas);
+
+// Call resizeCanvas initially to set the canvas size
+resizeCanvas();
 
 function generateNoise() {
-    for (let i = 0; i < data.length; i += 3) {
-        const gray = Math.random() * 255;
-        data[i] = gray;     // red
-        data[i + 1] = gray; // green
-        data[i + 2] = gray; // blue
-        data[i + 3] = 255;  // alpha
+    for (let y = 0; y < canvas.height; y += gridHeight) {
+        for (let x = 0; x < canvas.width; x += gridWidth) {
+            // Apply offset to every other row
+            let xOffset = (y / gridHeight) % 2 === 0 ? x : x + offset;
+            
+            // Generate a mark at each grid point
+            for (let j = 0; j < markHeight; j++) {
+                for (let i = 0; i < markWidth; i++) {
+                    const index = ((y + j) * canvas.width + (xOffset + i)) * 4;
+                    data[index] = markColor[0];     // red
+                    data[index + 1] = markColor[1]; // green
+                    data[index + 2] = markColor[2]; // blue
+                    data[index + 3] = markColor[3] * markOpacity;  // alpha
+                }
+            }
+        }
     }
 }
 
@@ -22,7 +53,5 @@ function animateNoise() {
 }
 
 animateNoise();
-
-
 
 
